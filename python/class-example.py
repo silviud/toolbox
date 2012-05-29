96df62a6827ced31744e167236ca7d5fbb067893
Kegan Holtzhausen <unixunion@gmail.com>
2012-05-29 15:50:55 +0200

    correct syntax for mysql example connector

--- python/class-example.py
+++ python/class-example.py
@@ -2,6 +2,6 @@
 from myclass import daSql
 list = []
 while True:
-   a = daSql("somehost:3306",username="myusername",password="mypassword")
+   a = daSql("mysql://hostname/dbname",username="myusername",password="mypassword")
    a.connect()
    list.append(a)
